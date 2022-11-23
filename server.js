ec598a1a46326978584b360c4187721a4cb8a7d4
ch04937 <97hernandez.c@gmail.com>
2022-11-22 20:28:50 -0800

    add hardcoded content

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const userRouter = require("./src/router/user");
 const contactRouter = require("./src/router/contactMe");
 const calendarRouter = require("./src/router/calendar");
 const galleryRouter = require("./src/router/gallery");
-const servicesRouter = require("./src/router/services");
 const glamourellaRouter = require("./src/router/glamourella");
 
 // env
@@ -28,7 +27,6 @@ app.use(cors({ credentials: true, origin: clientURL }));
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/contact-me", contactRouter);
-app.use("/services", servicesRouter);
 app.use("/calendar", calendarRouter);
 app.use("/gallery", galleryRouter);
 app.use("/glamourella", glamourellaRouter);
--- src/router/glamourella.js
+++ src/router/glamourella.js
@@ -14,7 +14,7 @@ const socials = [
   {
     social: "instagram",
     link: "https://www.instagram.com/glamourrella/",
-    key: v4(),
+    uid: v4(),
   },
 ];
 const about = [
@@ -37,9 +37,62 @@ const about = [
     lines: 3,
   },
 ];
+const services = [
+  {
+    uid: v4(),
+    type: "pedicure",
+    src: "http://localhost:4937/gallery/photo/?path=assets/pedicure/billie-unsplash.jpg",
+    name: "classic ",
+    length: "30 minutes",
+    cost: 25,
+    description:
+      "Your feet will be pampered, nails are professionally shaped and filed, cuticle trimmer, callus removed, followed by a hot oil massage and finish with a nail buff or polish.",
+  },
+  {
+    uid: v4(),
+    type: "pedicure",
+    src: "http://localhost:4937/gallery/photo/?path=assets/pedicure/billie-unsplash.jpg",
+    name: "REFRESHER ",
+    length: "45 minutes",
+    cost: 35,
+    description:
+      "A combination of our Classic Pedicure with an exfoliation pink salt, deep massage with hot stones, warm towel wrap. Finish with a nail buff or polish.",
+  },
+  {
+    uid: v4(),
+    type: "manicure",
+    src: "http://localhost:4937/gallery/photo/?path=assets/manicure/rashid-khreiss-unsplash.jpg",
+    name: "classic ",
+    length: "20 minutes",
+    cost: 15,
+    description:
+      "Your nail will be professionally shaped and filed,  cuticle trimmer. Followed by lotion massage and finish with a nail buff or polish.",
+  },
+  {
+    uid: v4(),
+    type: "manicure",
+    src: "http://localhost:4937/gallery/photo/?path=assets/manicure/bryony-elena-unsplash.jpg",
+    name: "refresher ",
+    length: "30 minutes",
+    cost: 25,
+    description:
+      "A combination of Classic Manicure, your hand will be soaked in moisturized lotion gloves and rubbed with an exfoliation pink salt. Follow by a deep massage with hot stones, warm towel wrap and finish with a nail buff or polish.",
+  },
+  {
+    uid: v4(),
+    type: "manicure",
+    src: "http://localhost:4937/gallery/photo/?path=assets/manicure/kris-atomic-unsplash.jpg",
+    name: "paris signature ",
+    length: "56 minutes",
+    cost: 29,
+    description:
+      "Your nail will be professionally shaped and filed,  cuticle trimmer. Followed by lotion massage and finish with a nail buff or polish.",
+  },
+];
+
 router.get("/", async (req, res) => {
   try {
-    res.status(200).json({ socials, about });
+    res.status(200).json({ socials, about, services });
   } catch {
     res.status(500).json(errMail);
   }
