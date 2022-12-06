7f569f2708dde7951e9c0ef32c00d387e47de9ef
ch04937 <97hernandez.c@gmail.com>
2022-12-06 01:45:28 -0800

    send refreshtoken

--- src/middleware/validateCookie.js
+++ src/middleware/validateCookie.js
@@ -2,13 +2,11 @@ const jwt = require("jsonwebtoken");
 const { cookieName, accessSecret } = require("../../config");
 
 module.exports = async (req, res, next) => {
-  const { accessToken } = req.cookies[cookieName];
-  if (accessToken === undefined || accessToken === "undefined") {
+  const token = req.cookies[cookieName];
+  if (token === undefined || token === "undefined") {
     return res.status(400).json("");
   }
-  console.log("token", accessSecret);
-  let payload = jwt.verify(accessToken, accessSecret);
+  let payload = jwt.verify(token, accessSecret);
   req.user = payload;
-
   next();
 };
